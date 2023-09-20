59971f02dd02209876fa387ead2df05fb6ad0091
Manuel OG <manuelalejo429@gmail.com>
2023-09-20 14:58:30 -0500

    Use of JOIN clause - Inclusive and Exclusive way methods

--- practicing.sql
+++ practicing.sql
@@ -359,6 +359,84 @@ FROM (
 ) AS alumnosTutor;
 
 
+SELECT a.nombre,
+	   a.apellido,
+	   a.carrera_id,
+	   c.id,
+	   c.carrera
+FROM platzi.alumnos AS a
+	LEFT JOIN platzi.carreras AS c
+	ON a.carrera_id = c.id
+WHERE c.id IS NULL 
+ORDER BY a.carrera_id;
+	   	
+		
+SELECT a.nombre,
+	   a.apellido,
+	   a.carrera_id,
+	   c.id,
+	   c.carrera
+FROM platzi.alumnos AS a
+	LEFT JOIN platzi.carreras AS c
+	ON a.carrera_id = c.id
+WHERE c.id IS NULL  OR c.id IS NOT NULL  
+ORDER BY a.carrera_id;
+	   	
+
+SELECT a.nombre,
+	   a.apellido,
+	   a.carrera_id,
+	   c.id,
+	   c.carrera
+FROM platzi.alumnos AS a
+	LEFT JOIN platzi.carreras AS c
+	ON a.carrera_id = c.id
+ORDER BY a.carrera_id;
 
 
+SELECT a.nombre,
+	   a.apellido,
+	   a.carrera_id,
+	   c.id,
+	   c.carrera
+FROM platzi.alumnos AS a
+FULL OUTER JOIN platzi.carreras AS c
+	ON a.carrera_id = c.id
+ORDER BY a.carrera_id;
 
+SELECT a.nombre,
+	   a.apellido,
+	   a.carrera_id,
+	   c.id,
+	   c.carrera
+FROM platzi.alumnos AS a
+	RIGHT JOIN platzi.carreras AS c
+	ON a.carrera_id = c.id
+WHERE a.carrera_id  IS NULL
+ORDER BY c.id DESC; 
+
+
+SELECT a.nombre,
+	   a.apellido,
+	   a.carrera_id,
+	   c.id,
+	   c.carrera
+FROM platzi.alumnos AS a
+	INNER JOIN platzi.carreras AS c
+	ON a.carrera_id = c.id
+ORDER BY c.id; 
+ 
+
+SELECT a.nombre,
+	   a.apellido,
+	   a.carrera_id,
+	   c.id,
+	   c.carrera
+FROM platzi.alumnos AS a
+	FULL OUTER JOIN platzi.carreras AS c
+	ON a.carrera_id = c.id
+WHERE (a.carrera_id IS NULL ) OR (c.id IS NULL)
+ORDER BY c.id; 
+  
+ 
+ 
